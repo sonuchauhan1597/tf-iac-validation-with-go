8a48682494c1eeaa5c0264441f926bc5736e9eab
apple <apple@apples-MacBook-Pro.local>
2025-05-09 02:36:59 +0530

    update: github workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,9 @@ provider "aws" {
   secret_key                  = var.use_localstack ? "test" : null
   skip_credentials_validation = var.use_localstack
   skip_metadata_api_check     = var.use_localstack
-  
+  skip_requesting_account_id  = var.use_localstack
+
+
   endpoints {
     s3       = var.use_localstack ? "http://localhost:4566" : null
     dynamodb = var.use_localstack ? "http://localhost:4566" : null
